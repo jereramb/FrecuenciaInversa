f01499138105982f86f7be2859dad1ee30355415
unknown <jere-ramb@outlook.com>
2016-04-24 12:14:24 -0300

    Cambios mayores

--- resources/js/main.js
+++ resources/js/main.js
@@ -10,15 +10,15 @@ $(document).ready(function(){
 		zoom: 2
 	});
 
-	newMarker(-32.9543499, -60.6580895, "Rosario", "i");
-	newMarker(-23.6029336, -46.641568, "San Pablo", "i");
-	newMarker(48.8586329, 2.3218811, "Paris", "i");
-	newMarker(35.7117067, 139.6953929, "Tokio", "i");
-	newMarker(55.7532508, 37.561263, "Moscu", "i");
-	newMarker(40.7247955, -74.0101607, "New York", "i");
-	newMarker(-33.8653142, 151.1944544, "Sidney", "i");
-	newMarker(52.5156735, 13.3430367, "Berlin", "i");
-	newMarker(-26.2024546, 28.0162336, "Johannesburgo", "i");
+	newMarker(-32.9543499, -60.6580895, "Rosario", "<p><h2>Rosario</h2></p>Calidad del Aire: <b>Optima</b><p></p><p><u>Parametros</u></p><p>PM10: 30</p><p>[O3]: 16</p><p>[SO2]: 12</p><p>[CO]: 0</p><p>Presion: 1026</p><p>Humedad: 100</p><p></p><p>Casos de <u>Tos</u> relevados: <b>10</b></p>", 1);
+	newMarker(40.7247955, -74.0101607, "New York", "<p><h2>New York</h2></p>Calidad del Aire: <b>Optima</b><p></p><p><u>Parametros</u></p><p>PM10: </p><p>[O3]: 29</p><p>[NO2]: 15</p><p>[SO2]: 1</p><p>[CO]: 4</p><p>Presion: 990</p><p>Humedad: 97</p><p></p><p>Casos de <u>Tos</u> relevados: <b>5</b></p>", 1);
+	newMarker(-33.8653142, 151.1944544, "Sidney", "<p><h2>Sidney</h2></p>Calidad del Aire: <b>Optima</b><p></p><p><u>Parametros</u></p><p>PM10: 21</p><p>[O3]: 30</p><p>[NO2]: 18</p><p>[SO2]: 16</p><p>[CO]: 5</p><p>Presion: 1030</p><p>Humedad: 100</p><p></p><p>Casos de <u>Tos</u> relevados: <b>8</b></p>", 1);
+	newMarker(48.8586329, 2.3218811, "Paris", "<p><h2>Paris</h2></p>Calidad del Aire: <b>Parcial</b><p></p><p><u>Parametros</u></p><p>PM10: 61</p><p>[O3]: 32</p><p>[NO2]: 29</p><p>[SO2]: 2</p><p>[CO]: 0</p><p>Presion: 1025</p><p>Humedad: 93</p><p></p><p>Casos de <u>Tos</u> relevados: <b>24</b></p>", 2);
+	newMarker(55.7532508, 37.561263, "Moscu", "<p><h2>Moscú</h2></p>Calidad del Aire: <b>Parcial</b><p></p><p><u>Parametros</u></p><p>PM10: 30</p><p>[O3]: </p><p>[NO2]: 42</p><p>[SO2]: 2</p><p>[CO]: 3</p><p>Presion: 1012</p><p>Humedad: 100</p><p></p><p>Casos de <u>Tos</u> relevados: <b>20</b></p>", 2);
+	newMarker(52.5156735, 13.3430367, "Berlin", "<p><h2>Berlin</h2></p>Calidad del Aire: <b>Parcial</b><p></p><p><u>Parametros</u></p><p>PM10: 51</p><p>[O3]: 32</p><p>[NO2]: 64</p><p>[SO2]: </p><p>[CO]: </p><p>Presion: 1019</p><p>Humedad: 87</p><p></p><p>Casos de <u>Tos</u> relevados: <b>28</b></p>", 2);
+	newMarker(-26.2024546, 28.0162336, "Johannesburgo", "<p><h2>Johannesburgo</h2></p>Calidad del Aire: <b>Parcial</b><p></p><p><u>Parametros</u></p><p>PM10: 82</p><p>[O3]: </p><p>[NO2]: </p><p>[SO2]: </p><p>[CO]: </p><p>Presion: 1032</p><p>Humedad: 89</p><p></p><p>Casos de <u>Tos</u> relevados: <b>33</b></p>", 2);
+	newMarker(35.7117067, 139.6953929, "Tokio", "<p><h2>Tokio</h2></p>Calidad del Aire: <b>No Optima</b><p></p><p><u>Parametros</u></p><p>PM10: 45</p><p>[O3]: 50</p><p>[NO2]: 26</p><p>[SO2]: 7</p><p>[CO]: 3</p><p>Presion: 1022</p><p>Humedad: 92</p><p></p><p>Casos de <u>Tos</u> relevados: <b>31</b></p>", 3);
+	newMarker(-23.6029336, -46.641568, "San Pablo", "<p><h2>San Pablo</h2></p>Calidad del Aire: <b>No Optima</b><p></p><p><u>Parametros</u></p><p>PM10: 116</p><p>[O3]: 54</p><p>[NO2]: 28</p><p>[SO2]: 8</p><p>[CO]: 21</p><p>Presion: 1029</p><p>Humedad: 100</p><p></p><p>Casos de <u>Tos</u> relevados: <b>40</b></p>", 3);
 
 	$("#edadDropdown>li").on('click', function(event) {
 		event.preventDefault();
@@ -37,11 +37,26 @@ $(document).ready(function(){
 	});
 
 	$("#SendInfo").on('click', submitInfo);
+
+	new Chartist.Line('.md-chart', {
+		labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61],
+		series: [[ 0.47884, 0.49799, 0.5363, 0.68953, 0.55545, 0.47884, 0.43096, 0.38307, 0.3735, 0.06704, -0.3735, -0.07661, 0.60334, 1.01514, 1.28329, 1.39821, 1.06303, 0.64165, 0.38307, 0.05746, 0.1245, 0.34477, 0.63207, 0.6608, 0.62249, 0.51715, 0.69911, 0.71826, 0.67038, 0.05746, 0.10534, 0.5363, 1.15879, 0.97683, 0.62249, 0.33519, 0.17238, 0.10534, 0.19154, 0.3735, 0.82361, 0.82361, 0.71826, 0.52672, 0.49799, 0.58419, 0.63207, 0.64165, 0.56503, 0.55545, 0.48842, 0.43096, 0.33519, 0.33519, 0.34477, 0.36392, 0.40223, 0.4118, 0.43096, 0.45011, 0.47884, 0.48842]]
+	},
+	{
+		fullWidth: true,
+		chartPadding:{
+			right: 10
+		}
+	});
 });
 
-function newMarker(latArg, longArg, title, contenido) {
+function newMarker(latArg, longArg, title, contenido, tipo) {
 	pos = {lat: latArg, lng: longArg};
 	var markerImage = 'resources/images/markerMaps.png';
+	if(tipo == 1){markerImage = 'resources/images/markerMapsVerde.png';}
+	if(tipo == 2){markerImage = 'resources/images/markerMapsAmarillo.png';}
+	if(tipo == 3){markerImage = 'resources/images/markerMapsRojo.png';}
+	
 
 	var infowindow = new google.maps.InfoWindow({
 		content: contenido
